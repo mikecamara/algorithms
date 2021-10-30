82426e451b2f2e2cd199e76c291505f81a867be4
Mike Camara <mcmikecamara@gmail.com>
2021-10-30 23:25:17 +0300

    Lonely Element Tests Passing

--- src/lonely-element/LonelyElement.js
+++ src/lonely-element/LonelyElement.js
@@ -1,5 +1,25 @@
 function lonelyinteger(a) {
     // Write your code here
-    console.log(a)
-
+    var testedElements = [];
+    var duplidatedElements = [];
+    var soloElement = null;
+    for(var i=0; i < a.length; i++) {
+        if (testedElements.includes(a[i])){
+            duplidatedElements.push(a[i]);
+        } else {
+            testedElements.push(a[i])
+        } 
+    }
+    for(var i=0; i < a.length; i++) {
+        if (duplidatedElements.includes(a[i])){
+            continue;
+        } else {
+            soloElement = a[i];
+            break;
+        } 
+    }   
+    console.log(soloElement);
+    return soloElement;
 }
+
+export default lonelyinteger;
--- src/lonely-element/__test__/LonelyElement.test.js
+++ src/lonely-element/__test__/LonelyElement.test.js
@@ -3,6 +3,6 @@ import lonelyinteger from "../LonelyElement";
 
 describe("Outputs the lonely element of the list", ()=>{
     test("Is there a lonely element in the list", ()=>{
-        expect(lonelyinteger([1,1,2,2,3,3,4]).toBe(4))
+        expect(lonelyinteger([1,1,2,2,3,3,4])).toStrictEqual(4)
     })
 })
