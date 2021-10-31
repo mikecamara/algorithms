b0cead745d5400e6ec6ec17f19e79b74f37b1dd9
Mike Camara <mcmikecamara@gmail.com>
2021-10-31 19:29:16 +0200

    Anagram algorithm tests passing

--- src/anagram/Anagram.js
+++ src/anagram/Anagram.js
@@ -1,7 +1,29 @@
 function isAnagram(word1, word2){
 
-    
+    if (word1.length !== word2.length){
+        return false;
+    }
 
+    let letterCounter = {};
+
+    for (let letter of word1){
+        if (letterCounter[letter] === undefined){
+            letterCounter[letter] = 1;
+        } else {
+            letterCounter[letter]++;
+        }
+    }
+
+    for (let letter of word2){
+        if (letterCounter[letter] === undefined){
+            return false;
+        }
+        if (letterCounter[letter] < 1){
+            return false;
+        }
+        letterCounter[letter]--;
+    }
+    return true;
 }
 
 export default isAnagram;
--- src/anagram/__test__/Anagram.test.js
+++ src/anagram/__test__/Anagram.test.js
@@ -5,4 +5,9 @@ describe("Is Anagram test", ()=>{
     test("Is these two words a anagram", ()=>{
         expect(isAnagram("iceman", "cinema")).toBe(true);
     })
+
+    test("This is not an anagram is must fail", ()=>{
+        expect(isAnagram("cat", "dog")).toBe(false);
+    })
 })
+
